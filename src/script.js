9274e8a891718c02932af50710d68e5a61f796b7
Jioh19 <jioh1982@gmail.com>
2023-10-02 23:40:31 -0300

    add cards

--- src/script.js
+++ src/script.js
@@ -4,17 +4,31 @@ const APIKEY =
 function getLeagues() {
 	const ligas = document.querySelector('.ligas');
 	const dir =
-		'https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=' + APIKEY;
+		'https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=' +
+		APIKEY +
+		'&from=2023-09-20&to=2023-10-01';
 	fetch(dir)
 		.then((response) => response.json())
 		.then((league) => {
 			league.result.forEach((liga) => {
 				console.log(liga);
-				ligas.innerHTML += `<img src="${
-					liga.league_logo
-				}" alt="Logo"> <img src="${
-					liga.country_logo
-				}" alt="Country"> ${JSON.stringify(league)}`;
+
+				ligas.innerHTML += `
+          <div class="card">
+            <div class="card-header">
+              <div class="card-inner">
+                <img class="logo" src="${liga.home_team_logo}" alt="Home">
+                <p>${liga.event_home_team}</p>
+              </div>
+              <div class="card-inner">
+                <img class="logo" src="${liga.league_logo}" alt="League">
+                <p>${liga.event_ft_result}</p>
+              </div>
+              <div class="card-inner">
+                <img class="logo" src="${liga.away_team_logo}" alt="Away">
+                <p>${liga.event_away_team}</p>
+              </div>
+          </div>`;
 			});
 		})
 		.catch((err) => {
