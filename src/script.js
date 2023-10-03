ea62fbfc6d5f085b796114186990aa93a74315dc
Jioh19 <jioh1982@gmail.com>
2023-10-02 22:13:30 -0300

    add logos

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const APIKEY =
 	'19cc466a8fc23dcf4f07faec1acb691816d2d1fe224c08f50d0824943e5ed30a';
+
 function getLeagues() {
 	const ligas = document.querySelector('.ligas');
 	const dir =
@@ -7,7 +8,14 @@ function getLeagues() {
 	fetch(dir)
 		.then((response) => response.json())
 		.then((league) => {
-			ligas.innerHTML += `${JSON.stringify(league)}`;
+			league.result.forEach((liga) => {
+				console.log(liga);
+				ligas.innerHTML += `<img src="${
+					liga.league_logo
+				}" alt="Logo"> <img src="${
+					liga.country_logo
+				}" alt="Country"> ${JSON.stringify(league)}`;
+			});
 		})
 		.catch((err) => {
 			return Promise.reject(Error(err.message));
